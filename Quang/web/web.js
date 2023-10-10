3509d4359a62d77167a6377289baafe0ad3aedbb
Quang Nguyễn <quang030803@gmail.com>
2023-10-10 17:05:19 +0700

    final commit

--- Quang/web/web.js
+++ Quang/web/web.js
@@ -8,10 +8,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
       });
     });
   
-    const blocks = document.querySelectorAll('.tinanh, .tinchu');
+    const img_blocks = document.querySelectorAll('.tinanh');
+    const text_blocks = document.querySelectorAll('.tinchu');
   
-    blocks.forEach(block => {
-        observer.observe(block);
+    img_blocks.forEach((block) => {
+      observer.observe(block);
+      text_blocks.forEach((bl) => {
+        setTimeout(function(){
+          observer.observe(bl)
+        }, 400);
+      });
     });
 });
   
@@ -64,4 +70,40 @@ document.addEventListener('scroll', (event) => {
       observer.observe(map);
     }, (index + 1) * 500);
   });
-})
+});
+
+
+function displayContent(id, buttom) {
+  var i, contentBlocks, listButtom;
+  contentBlocks = document.getElementsByClassName("display");
+  listButtom = document.getElementsByClassName("buttom")
+  for (i = 0; i < contentBlocks.length; i++) {
+      contentBlocks[i].style.display = "none";
+      listButtom[i].style.backgroundColor = "#7996A4";
+  }
+  document.getElementById(id).style.display = "flex";
+  document.getElementById(buttom).style.backgroundColor = "#0A3643"
+};
+
+document.addEventListener('scroll', (event) => {
+  let observer = new IntersectionObserver(function(entries, observer) {
+    entries.forEach((entry) => {
+      if(entry.isIntersecting) {
+        entry.target.classList.add('show');
+        observer.unobserve(entry.target);
+      }
+    });
+  });
+
+  const img_blocks = document.querySelectorAll('.tinanh1');
+  const text_blocks = document.querySelectorAll('.tinchu1');
+
+  img_blocks.forEach((block) => {
+    observer.observe(block);
+    text_blocks.forEach((bl) => {
+      setTimeout(function(){
+        observer.observe(bl)
+      }, 400);
+    });
+  });
+});
